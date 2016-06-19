ccff0c0522497e5c8f4559d79df6a3670cb00c99
Matthew Balch <matbalch@vt.edu>
2016-06-19 15:27:11 -0400

    Press M to mute

--- js/init.js
+++ js/init.js
@@ -207,6 +207,26 @@ function init() {
                 menuScreen.unpause();
             }));
 
+    function renderMuteButton(state_function, ctx, time, dt) {
+        ctx.beginPath();
+        ctx.strokeStyle = "#EEEEEE";
+        ctx.lineWidth = 2;
+        ctx.arc(0, 0, 25, 0, Math.PI * 2);
+        if(!state_function()) {
+            var p = 25 / Math.sqrt(2);
+            ctx.moveTo(-p, -p);
+            ctx.lineTo(p, p);
+        }
+        ctx.stroke();
+    }
+
+    var muteEffectsButton = new Button(
+        Vector.create(675, 170),
+        new BoundingCircle(25),
+        game.toggleEffects.bind(game));
+    muteEffectsButton.render = renderMuteButton.bind(null, game.hasEffects.bind(game));
+    optionScreen.addOption(muteEffectsButton);
+
     optionScreen.addOption(
         new TextButton(
             Vector.create(100, 150),
@@ -225,9 +245,16 @@ function init() {
         50,
         100);
 
+    var muteMusicButton = new Button(
+        Vector.create(675, 320),
+        new BoundingCircle(25),
+        game.toggleMusic.bind(game));
+    muteMusicButton.render = renderMuteButton.bind(null, game.hasMusic.bind(game));
+    optionScreen.addOption(muteMusicButton);
+
     optionScreen.addOption(
         new TextButton(
-            Vector.create(100, 250),
+            Vector.create(100, 300),
             "Music",
             "24px joystix",
             "#CCCCCC",
@@ -235,7 +262,7 @@ function init() {
             }));
 
     optionScreen.addSlider(
-        Vector.create(100, 300),
+        Vector.create(100, 350),
         game.getMusicVolume.bind(game),
         game.setMusicVolume.bind(game),
         600,
