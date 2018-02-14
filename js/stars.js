e5c6776d500c30aa60d4b1dad3936ab12621f618
dfirebaugh <dafirebaugh@gmail.com>
2018-02-13 20:18:18 -0500

    refactoring the Star Object

--- js/stars.js
+++ js/stars.js
@@ -1,13 +1,9 @@
 "use strict";
 let stars = [];
-let numStars = 87;
 
-window.addEventListener("load", function() {
-  let canvas = document.getElementById('canvas');
-
-  // Add stars to the stars array
-  stars = Array.apply(null, Array(numStars)).map(()=>new Star());
-
+window.addEventListener("load", () => {
+  stars = populateStars(87)
+  // console.log(stars)
 }, false);
 
 let animate = (con, currentTime) => {
@@ -16,55 +12,64 @@ let animate = (con, currentTime) => {
   con.restore();
 }
 
-function Star() {
-  this.length = 1 + Math.random() * 1.8;
-  this.period = Math.random() * 1.6 + 10;
-  this.factor = 1;
-  this.period = Math.random() * 1.6 + 10;
-  this.cycle = 0;
-  this.pos = {
-    x: Math.round(Math.random() * canvas.width),
-    y: Math.round(Math.random() * canvas.height)
-  };
+let populateStars = (numStars) => {
+  let arr = Array.apply(null, Array(numStars)).map(() => Star.init())
+  return arr;
 }
 
-Star.prototype.draw = function(con, currentTime) {
-  let frameTime = (currentTime % this.period) / this.period;
+let Star = {
+  init() {
+    let newStar = Object.create(this);
+    newStar.length = 1 + Math.random() * 1.8;
+    newStar.period = Math.random() * 1.6 + 10;
+    newStar.factor = 1;
+    newStar.period = Math.random() * 1.6 + 10;
+    newStar.cycle = 0;
+    newStar.pos = {
+      x: Math.round(Math.random() * canvas.width),
+      y: Math.round(Math.random() * canvas.height)
+    }
+    return newStar;
+  },
+  draw(con, currentTime) {
 
-  let passedCycles = Math.floor(currentTime / this.period);
+    let frameTime = (currentTime % this.period) / this.period;
 
-  // Save the context
-  con.save();
+    let passedCycles = Math.floor(currentTime / this.period);
 
-  // move into the middle of the canvas, just to make room
-  con.translate(this.pos.x + this.pos.x / 2, this.pos.y);
+    // Save the context
+    con.save();
 
-  if(this.cycle < passedCycles) {
-        this.pos.x = Math.round(Math.random() * con.width);
-        this.pos.y = Math.round(Math.random() * con.width);
-        this.cycle = passedCycles;
+    // move into the middle of the canvas, just to make room
+    con.translate(this.pos.x + this.pos.x / 2, this.pos.y);
+
+    if (this.cycle < passedCycles) {
+      this.pos.x = Math.round(Math.random() * con.width);
+      this.pos.y = Math.round(Math.random() * con.width);
+      this.cycle = passedCycles;
     }
 
-  //TODO: have opacity change slowly between .2 and .5
-  //TODO: make opacity transition smoother between frames
-  this.opacity =  0.5 + 0.5 * Math.sin(2 * Math.PI * frameTime);
+    //TODO: have opacity change slowly between .2 and .5
+    //TODO: make opacity transition smoother between frames
+    this.opacity = 0.5 + 0.5 * Math.sin(2 * Math.PI * frameTime);
 
 
-  con.beginPath()
-  for (let i = 5; i > 0; i--) {
+    con.beginPath()
+    for (let i = 5; i > 0; i--) {
+      con.lineTo(0, this.length);
+      con.translate(0, this.length);
+      con.rotate((Math.PI * 2 / 10));
+      con.lineTo(0, -this.length);
+      con.translate(0, -this.length);
+      con.rotate(-(Math.PI * 6 / 10));
+    }
     con.lineTo(0, this.length);
-    con.translate(0, this.length);
-    con.rotate((Math.PI * 2 / 10));
-    con.lineTo(0, -this.length);
-    con.translate(0, -this.length);
-    con.rotate(-(Math.PI * 6 / 10));
-  }
-  con.lineTo(0, this.length);
-  con.closePath();
-  con.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
-  con.shadowBlur = 5;
-  con.shadowColor = '#ffff33';
-  con.fill();
+    con.closePath();
+    con.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
+    con.shadowBlur = 5;
+    con.shadowColor = '#ffff33';
+    con.fill();
 
-  con.restore();
+    con.restore();
+  }
 }
